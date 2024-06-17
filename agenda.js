f644c841ed723801a8de7528e6bf5c30ec3392e9
Cauã Andrade <caualsantos@hotmail.com>
2024-06-17 03:18:43 -0300

    feat(agenda): adicao do campo sobre na pagina de agenda

--- agenda.js
+++ agenda.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskForm = document.getElementById('taskForm');
     const taskInput = document.getElementById('taskInput');
+    const taskAboutInput = document.getElementById('taskAboutInput');
     const taskDate = document.getElementById('taskDate');
     const taskList = document.getElementById('taskList');
 
     taskForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const taskText = taskInput.value.trim();
+        const taskAboutText = taskAboutInput.value.trim();
         const taskDueDate = taskDate.value;
-        if (taskText && taskDueDate) {
+        if (taskText && taskDueDate && taskAboutInput) {
             const li = document.createElement('li');
-            li.innerHTML = `${taskText} - <em>${taskDueDate}</em> <button class="completeBtn">Concluir</button>`;
+            li.innerHTML = `<em>${taskDueDate}</em> <b>${taskText}</b> ${taskAboutText} <button class="completeBtn">Concluir</button>`;
             taskList.appendChild(li);
             taskInput.value = '';
+            taskAboutInput.value = '';
             taskDate.value = '';
         }
     });
